148826e515b91e206d6264e6e8b610d45086e1e6
Jatin Chauhan <artist.jin.98@gmail.com>
2026-07-15 11:26:15 +0530

    add static html and css files to make the UI better

--- app/main.py
+++ app/main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 from app.api.v1.api import api_router
 from fastapi.responses import RedirectResponse
 from app.core.config import settings
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(title=settings.PROJECT_NAME, version=settings.VERSION)
 app.include_router(api_router, prefix="/api/v1")
 
+app.mount("/", StaticFiles(directory="app/static", html=True))
 
-@app.get("/", include_in_schema=False)
-def root():
-    return RedirectResponse(url="/api/v1/calculator")
+# @app.get("/", include_in_schema=False)
+# def root():
+#     return RedirectResponse(url="/api/v1/calculator")
